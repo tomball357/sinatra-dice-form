89a75e60b31df83250f58947814868611048289f
tomball357 <tball@chicagobooth.edu>
2025-03-14 15:48:48 +0000

    new

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
+
+
 require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/dice/:number_of_dice/:how_many_sides") do
+  @num_dice = params.fetch("number_of_dice").to_i
+
+  @sides = params.fetch("how_many_sides").to_i
+  
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:process_roll)
 end
